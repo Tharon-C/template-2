bd57f24e0c51ed7be78f53651993fc056b294f18
Tharon Carlson <design@tharoncarlson.com>
2015-03-30 10:32:51 -0700

    update to README, dropdowns, helper classes, etc

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 #Template 1
 
-A Template using [HTML5 Blank](http://html5blank.com/) with a simple grid system. It it a project for developers at [Tagline Media Group](http://taglinegroup.com) to use for quick development of custom [Wordpress](https://wordpress.org) themes.
+A Template using [HTML5 Blank](http://html5blank.com/) with a simple grid system. It is a starter for developers at [Tagline Media Group](http://taglinegroup.com) to use for quick development of custom [Wordpress](https://wordpress.org) themes.
 
 ##Some Basic Features
-* Automation and preprocessing with Gulp and SASS
-* Short codes for making links and images more portable.
+* Starter database dump with style guide page.
+* [SASS](http://sass-lang.com/) based template structure leveraging usfull mixins and variables connected to the style guide. 
+* Automation and preprocessing with [Gulp](http://gulpjs.com/).
+* Shortcodes for making links and images more portable.
 * Added jQuery for quickly creating responsive Tabs & other fun stuff.
 
--- single.php
+++ single.php
@@ -2,13 +2,13 @@
 
 	<main role="main">
 	<!-- section -->
-	<section>
+	<section class="clear section">
 
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
 		<!-- article -->
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
+			<div class="p-wrap">
 			<!-- post thumbnail -->
 			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
 				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
@@ -18,7 +18,7 @@
 			<!-- /post thumbnail -->
 
 			<!-- post title -->
-			<h1>
+			<h1 class="title-1">
 				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
 			</h1>
 			<!-- /post title -->
@@ -26,7 +26,7 @@
 			<!-- post details -->
 			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
 			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
+			 <span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span> 
 			<!-- /post details -->
 
 			<?php the_content(); // Dynamic Content ?>
@@ -39,8 +39,8 @@
 
 			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
 
-			<?php comments_template(); ?>
-
+			<?php //comments_template(); ?> 
+		</div>
 		</article>
 		<!-- /article -->
 
@@ -61,7 +61,5 @@
 	</section>
 	<!-- /section -->
 	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
